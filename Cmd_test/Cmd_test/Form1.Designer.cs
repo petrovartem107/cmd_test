559403838a358d578c24b9f8947deba85977a8fa
LAPTOP-U10RCJLI\а715 <petrovartem353@gmail.com>
2022-05-16 21:20:11 +0300

    Sozdal RunCMD

--- Cmd_test/Cmd_test/Form1.Designer.cs
+++ Cmd_test/Cmd_test/Form1.Designer.cs
@@ -28,13 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(743, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 80);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Нажми";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(579, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(479, 239);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 65);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(363, 50);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(174, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(257, 57);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Push";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1087, 602);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Cmd_test/Cmd_test/Form1.cs
+++ Cmd_test/Cmd_test/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,43 @@ namespace Cmd_test
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RunCMD("time");
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RunCMD(textBox2.Text);
+        }
+        private void RunCMD(string input)
+        {
+            ProcessStartInfo info = new ProcessStartInfo()
+            {
+                FileName = "cmd",
+                Arguments = $"/c chcp 1251 & {input}",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true
+            };
+            
+            //ProcessStartInfo info2 = new ProcessStartInfo()
+            //{
+            //    FileName = "cmd",
+            //    //Arguments = $"/c chcp 65001 & {input}",
+            //    Arguments = $"/c {input}",
+            //    UseShellExecute = false,
+            //    CreateNoWindow = true,
+            //    RedirectStandardOutput = true
+            //};
+            Process process = Process.Start(info);
+            string x = process.StandardOutput.ReadToEnd();
+            //process = Process.Start(info2);
+            textBox1.Text = x.Replace("’ҐЄгй п Є®¤®ў п бва ­Ёж : 1251", "");
         }
     }
 }
